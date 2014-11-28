c8ff4edfd26d8a850edfaf03c0796835cf5ec0d1
kafukafienkov <bartekjmroz@gmail.com>
2014-11-28 17:59:15 +0100

    Squareup Retrofit and Picasso added, InstagramApi interface

--- app/src/main/java/com/bartek/example/instagramviewer/controller/InstagramApiService.java
+++ app/src/main/java/com/bartek/example/instagramviewer/controller/InstagramApiService.java
@@ -1,8 +1,16 @@
 package com.bartek.example.instagramviewer.controller;
 
+import retrofit.http.GET;
+import retrofit.http.Path;
+import retrofit.http.Query;
+
 /**
  * Created by Bartek on 2014-11-28.
  */
 public interface InstagramApiService {
-    
+
+    @GET("/users/{userId}/media/recent/")
+    void getUserPhotos(@Path("userId") long userId,
+                       @Query("client_id") String clientId,
+                       Callback<UserMedia> callback);
 }
